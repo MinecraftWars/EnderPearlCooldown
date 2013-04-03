60f8ac6cca55ccbdb384c72d32d2c475fe4e1cae
Justin Kaeser <jast@zno.de>
2013-04-03 21:58:02 +0200

    change whitespace to spaces

--- src/net/sqdmc/epc/EPC.java
+++ src/net/sqdmc/epc/EPC.java
@@ -1,5 +1,5 @@
 package net.sqdmc.epc;
- 
+
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -10,92 +10,96 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.MetricsLite;
- 
+
 public class EPC extends JavaPlugin {
-	
-	public static EPC EPC;
-	
-	private Logger log;
-	
-	// configuration section
-	
-	/** Cooldown of pearls in ms. */
-	long cooldown = 3000;
-	
-	/** Price of a pearl throw. */
-	double price = 0;
-	
-	/** Show cooldown message. */
-	boolean showMessage = true;
-	
-	/** Vault hook. */
-	Economy economy = null;
-	
+
+    public static EPC EPC;
+
+    private Logger log;
+
+    // configuration section
+
+    /** Cooldown of pearls in ms. */
+    long cooldown = 3000;
+
+    /** Price of a pearl throw. */
+    double price = 0;
+
+    /** Show cooldown message. */
+    boolean showMessage = true;
+
+    /** Vault hook. */
+    Economy economy = null;
+
 
     public void onEnable() {
-    	log = getLogger();
-    	EPC = this;
-    	
-    	saveDefaultConfig();
-    	
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(new EPCListener(), this);
-		
+        log = getLogger();
+        EPC = this;
+
+        saveDefaultConfig();
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new EPCListener(), this);
+
         getCommand("epc").setExecutor(new Commands());
-        
-        setupEconomy();
+
+        setupEconomy(); 
+
         loadConfig();
-        
+
         try {
             MetricsLite metrics = new MetricsLite(this);
             metrics.start();
         } catch (IOException e) {
-        	log.info("Failed to submit PluginMetrics stats");
+            log.info("Failed to submit PluginMetrics stats");
         }
     }
-    
+
     private boolean setupEconomy()
     {
-    	try {
-	        @SuppressWarnings("unchecked")
+        try {
+            @SuppressWarnings("unchecked")
             Class<Economy> c = (Class<Economy>) Class.forName("net.milkbowl.vault.economy.Economy");
-	        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(c);
-	        if (economyProvider != null) 
-	            economy = economyProvider.getProvider();
-	        
+            economy = getServer().getServicesManager().load(c);
+
         } catch (ClassNotFoundException e) {
-        	log.info("Unable to load Vault. Enderpearl throwing price is disabled.");
-	        price = 0;
+            // loading vault failed
+        }
+
+        if (economy != null)
+            log.info("Vault hooked. Enderpearl throwing price enabled.");
+        else {
+            log.info("Unable to load Vault. Enderpearl throwing price is disabled.");
+            price = 0;
         }
 
         return (economy != null);
     }
-    
+
     private void loadConfig() {
-    	reloadConfig();
-		Configuration config = getConfig();
-		cooldown = config.getLong("cooldown");
-		price = config.getDouble("price");
+        reloadConfig();
+        Configuration config = getConfig();
+        cooldown = config.getLong("cooldown");
+        price = config.getDouble("price");
     }
-    
-    
+
+
     private class Commands implements CommandExecutor {
 
-		@Override
-    	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-			
-			if (args.length >=1 && "reload".equalsIgnoreCase(args[0])) {
-				loadConfig();
-				sender.sendMessage("[EPC] Reloaded configuration!");
-				return true;
-			}
-			
-			return false;
-		}
-    	
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+            if (args.length >=1 && "reload".equalsIgnoreCase(args[0])) {
+                loadConfig();
+                sender.sendMessage("[EPC] Reloaded configuration!");
+                return true;
+            }
+
+            return false;
+        }
+
     }
 
 }
--- src/net/sqdmc/epc/EPCListener.java
+++ src/net/sqdmc/epc/EPCListener.java
@@ -13,79 +13,78 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EPCListener implements Listener{
-        
+
     private Map<Player,Long> lastThrow = new HashMap<Player,Long>();
-    
-    
-    
+
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerUseEP(PlayerInteractEvent e){   
-    	if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK || 
-    			e.getItem() == null || e.getItem().getType() != Material.ENDER_PEARL) {
-    		return;
-    	}
-    	
+        if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK || 
+                e.getItem() == null || e.getItem().getType() != Material.ENDER_PEARL) {
+            return;
+        }
+
         Long now = System.currentTimeMillis();
         Player player = e.getPlayer();
-        
+
         // may a player use pearls at all?
         if (!player.hasPermission("enderpearl.use")) {
-        	if (EPC.EPC.showMessage)
-        		player.sendMessage("You may not use ender pearls");
-        	
-        	e.setCancelled(true);
-        	return;
+            if (EPC.EPC.showMessage)
+                player.sendMessage("You may not use ender pearls");
+
+            e.setCancelled(true);
+            return;
         }
-        
+
         // apply cooldown to player?
-    	if (validthrow(player, now)) {
-    		if (!pay(player)) // player can't pay
-    			e.setCancelled(true);
-    		else // allow throw, set cooldown
-    			lastThrow.put(player, now);
-    	} else {
-    		e.setCancelled(true);
-    	}	
-    }
-    
-    /** Check if player needs to and can pay for a throw. */ 
-    private boolean pay(Player player) {
-    	if (!player.hasPermission("enderpearl.pay") || EPC.EPC.price == 0 || EPC.EPC.economy == null)
-    		return true;
-    	
-    	String name = player.getName();
-    	double price = EPC.EPC.price;
-    	boolean success = false;
-    	if (EPC.EPC.economy.has(name, price))
-    		success = EPC.EPC.economy.withdrawPlayer(name, price).transactionSuccess();
-    	
-    	if (!success && EPC.EPC.showMessage)
-    		player.sendMessage(ChatColor.RED + "Not enough money to throw pearl. Need at least " 
-    		+ EPC.EPC.price + " " + EPC.EPC.economy.currencyNamePlural());
-	    return success;
+        if (validthrow(player, now)) {
+            if (!pay(player)) // player can't pay
+                e.setCancelled(true);
+            else // allow throw, set cooldown
+                lastThrow.put(player, now);
+        } else {
+            e.setCancelled(true);
+        }	
     }
 
-	/** Return remaining cooldown in seconds. */
+    /** Check if player needs to and can pay for a throw. */ 
+    private boolean pay(Player player) {
+        if (!player.hasPermission("enderpearl.pay") || EPC.EPC.price == 0 || EPC.EPC.economy == null)
+            return true;
+
+        String name = player.getName();
+        double price = EPC.EPC.price;
+        boolean success = false;
+        if (EPC.EPC.economy.has(name, price))
+            success = EPC.EPC.economy.withdrawPlayer(name, price).transactionSuccess();
+
+        if (!success && EPC.EPC.showMessage)
+            player.sendMessage(ChatColor.RED + "Not enough money to throw pearl. Need at least " 
+                    + EPC.EPC.price + " " + EPC.EPC.economy.currencyNamePlural());
+        return success;
+    }
+
+    /** Return remaining cooldown in seconds. */
     private long remainingCooldown(Player player, long throwTime) {
         Long lastPlayerPearl = lastThrow.get(player);
         return (EPC.EPC.cooldown - (throwTime - lastPlayerPearl)) / 1000;
     }
-    
+
     /** Check if player is allowed to throw a pearl at this moment. */
     private boolean validthrow(Player player, long throwTime) {
-    	if (!player.hasPermission("enderpearl.cooldown"))
-    		return true; // no cooldown for this player
-    	 
-    	 
+        if (!player.hasPermission("enderpearl.cooldown"))
+            return true; // no cooldown for this player
+
+
         Long lastPlayerPearl = lastThrow.get(player);
 
         // for players with cooldown, check if cooldown has passed
         if (lastPlayerPearl == null || (throwTime - lastPlayerPearl) >= EPC.EPC.cooldown)
             return true;
-        
-		if (EPC.EPC.showMessage)
-    		player.sendMessage(ChatColor.RED + "Enderpearl cooldown remaining: " + remainingCooldown(player, throwTime) + " seconds.");
-        
+
+        if (EPC.EPC.showMessage)
+            player.sendMessage(ChatColor.RED + "Enderpearl cooldown remaining: " + remainingCooldown(player, throwTime) + " seconds.");
+
         return false;
     }
 }
