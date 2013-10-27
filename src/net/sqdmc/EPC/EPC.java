615dcf8f77d409ca72aad51bbe92a3d45777a9ae
squidicuz <jono324@gmail.com>
2013-10-27 08:36:08 -0400

    Some tweaks and formatting.  Build against 1.6.4

--- src/net/sqdmc/EPC/EPC.java
+++ src/net/sqdmc/EPC/EPC.java
@@ -1,5 +1,5 @@
 package net.sqdmc.epc;
- 
+
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
@@ -8,53 +8,49 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
- 
+
 public class EPC extends JavaPlugin {
-	
-	public static EPC EPC;
-	
-	@SuppressWarnings("unused")
-	private Logger log;
-	
-	// configuration section
-	
-	/** Cooldown of pearls in ms. */
-	long cooldown = 3000;
-	
-	/** Show cooldown message. */
-	boolean showMessage = true;
-	
+
+    public static EPC EPC;
+    @SuppressWarnings("unused")
+    private Logger log;
+
+    // configuration section
+    /** Cooldown of pearls in ms. */
+    long cooldown = 3000;
+
+    /** Show cooldown message. */
+    boolean showMessage = true;
 
     public void onEnable() {
-    	log = getLogger();
-    	EPC = this;
-    	
-    	saveDefaultConfig();
-    	
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(new EPCListener(), this);
-		
+        log = getLogger();
+        EPC = this;
+
+        saveDefaultConfig();
+        Configuration config = getConfig();
+        cooldown = config.getLong("cooldown");
+        showMessage = config.getBoolean("showMessage");
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new EPCListener(), this);
+
         getCommand("epc").setExecutor(new Commands());
     }
-    
-    
+
     private class Commands implements CommandExecutor {
 
-		@Override
-    	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-			
-			if (args.length >=1 && "reload".equalsIgnoreCase(args[0])) {
-				reloadConfig();
-				Configuration config = getConfig();
-				cooldown = config.getLong("cooldown");
-				
-				sender.sendMessage("[EPC] Reloaded configuration!");
-				return true;
-			}
-			
-			return false;
-		}
-    	
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+            if (args.length >=1 && "reload".equalsIgnoreCase(args[0])) {
+                reloadConfig();
+                Configuration config = getConfig();
+                cooldown = config.getLong("cooldown");
+                showMessage = config.getBoolean("showMessage");
+                sender.sendMessage("[EPC] Reloaded configuration!");
+                return true;
+            }
+            return false;
+        }
     }
 
 }
--- src/net/sqdmc/EPC/EPCListener.java
+++ src/net/sqdmc/EPC/EPCListener.java
@@ -9,64 +9,92 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class EPCListener implements Listener{
-        
-    private Map<Player,Long> lastThrow = new HashMap<Player,Long>();
-    
-    
-    
+
+    private Map<String, Long> lastThrow = new HashMap<String, Long>();
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        checkPlayer(player.getName());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        checkPlayer(player.getName());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        if (lastThrow.containsKey(player.getName())) {
+            lastThrow.remove(player.getName());
+        }
+    }
+
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onPlayerUseEP(PlayerInteractEvent e){   
-    	if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK ) {
-    		return;
-    	}
-    	
-        Long now = System.currentTimeMillis();
-        Player player = e.getPlayer();
-        
+    public void onPlayerUseEP(PlayerInteractEvent e) {
+        if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK ) {
+            return;
+        }
+
+        final Player player = e.getPlayer();
+
         // may a player use pearls at all?
         if (!player.hasPermission("enderpearl.use")) {
-        	if (EPC.EPC.showMessage)
-        		player.sendMessage("You may not use ender pearls");
-        	
-        	e.setCancelled(true);
-        	return;
+            if (EPC.EPC.showMessage) {
+                player.sendMessage("You may not use ender pearls");
+            }
+            e.setCancelled(true);
+            return;
         }
-        
-        // apply cooldown to player?
-        if(e.getItem() != null && e.getItem().getType() == Material.ENDER_PEARL  && !validthrow(player, now)){
-        	// Display message and cooldown time.
-        	if (EPC.EPC.showMessage)
-        		player.sendMessage(ChatColor.RED + "Enderpearl cooldown remaining: " + remainingCooldown(player, now) + " seconds.");
-        	
-        	e.setCancelled(true);
+
+        // apply cooldown to a player
+        final Long now = System.currentTimeMillis();
+        if(e.getItem() != null && e.getItem().getType().equals(Material.ENDER_PEARL)  && !validthrow(player, now)) {
+            // Display message and cooldown time.
+            if (EPC.EPC.showMessage) {
+                player.sendMessage(ChatColor.RED + "Enderpearl cooldown remaining: ~" + remainingCooldown(player.getName(), now) + " seconds.");
+            }
+            e.setCancelled(true);
         }
     }
-    
+
     /** Return remaining cooldown in seconds. */
-    private long remainingCooldown(Player player, long throwTime) {
-        Long lastPlayerPearl = lastThrow.get(player);
+    private long remainingCooldown(String player, final long throwTime) {
+        final Long lastPlayerPearl = lastThrow.get(player);
         return (EPC.EPC.cooldown - (throwTime - lastPlayerPearl)) / 1000;
     }
-    
+
     /** Check if player is allowed to throw a pearl at this moment. */
     private boolean validthrow(Player player, long throwTime) {
-    	if (!player.hasPermission("enderpearl.cooldown"))
-    		return true; // no cooldown for this player
-    	 
-    	 
-        Long lastPlayerPearl = lastThrow.get(player);
+        if (!player.hasPermission("enderpearl.cooldown")) {
+            return true; // no cooldown for this player
+        }
+
+        Long lastPlayerPearl = lastThrow.get(player.getName());
 
         // for players with cooldown, check if cooldown has passed
-        if (lastPlayerPearl == null || (throwTime - lastPlayerPearl) >= EPC.EPC.cooldown) {
-        	
-            lastThrow.put(player, throwTime);
+        if (lastPlayerPearl == null || (throwTime - lastPlayerPearl) > EPC.EPC.cooldown) {
+            lastThrow.put(player.getName(), throwTime);
             return true;
-        } 
-        
+        }
         return false;
     }
+
+    private void checkPlayer(String name) {
+        if (lastThrow.containsKey(name)) {
+            if (remainingCooldown(name, System.currentTimeMillis()) <= 0) {
+                lastThrow.remove(name);
+            }
+        }
+    }
+
 }
